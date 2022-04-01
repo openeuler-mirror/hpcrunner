042439496def9687838fedf07f44edfe3e738b89
iotwins <fangchunlin@huawei.com>
2022-04-01 16:29:06 +0800

    fix git ignore issue

--- src/configService.py
+++ src/configService.py
@@ -10,9 +10,10 @@ class ConfigService:
         self.exe = ExecuteService()
         self.tool = ToolService()
         self.ROOT = os.getcwd()
+        self.meta_path = os.path.join(self.ROOT, DataService.meta_file)
 
     def switch_config(self, config_file):
         print(f"Switch config file to {config_file}")
-        meta_path = os.path.join(self.ROOT, DataService.meta_file)
-        self.tool.write_file(meta_path, config_file.strip())
+        self.tool.write_file(self.meta_path, config_file.strip())
         print("Successfully switched.")
+
