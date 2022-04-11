1a11f364a3f1199f6eb6e30ee93cb0ac7195ead4
chenzhiqiang <chenzhiqiang.work@foxmail.com>
2022-04-11 16:03:01 +0800

    add-fftw-gsl-lapack-libxc-scalapack

--- package/libxc/5.1.4/install.sh
+++ package/libxc/5.1.4/install.sh
@@ -4,6 +4,6 @@ set -e
 cd ${JARVIS_TMP}
 tar -xvf ${JARVIS_DOWNLOAD}/libxc-5.1.4.tar.gz
 cd libxc-5.1.4
-./configure FC=gfortran CC=gcc --prefix=$1
+./configure --prefix=$1 CC=clang CXX=clang++ FC=flang CFLAGS='-fPIC' FCFLAGS='-fPIC'
 make -j
 make install
