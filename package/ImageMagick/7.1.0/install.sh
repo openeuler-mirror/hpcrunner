003be94a5ba87d29712f8e49e69d6479fcbc1161
chenzhiqiang <chenzhiqiang.work@foxmail.com>
2022-04-19 16:44:03 +0800

    add-ImageMagick-ncview-netcdf

---
+++ package/ImageMagick/7.1.0/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# download from https://codeload.github.com/ImageMagick/ImageMagick/tar.gz/refs/tags/7.1.0-30
+set -x
+set -e
+
+cd ${JARVIS_TMP}
+tar -xvf ${JARVIS_DOWNLOAD}/ImageMagick-7.1.0-30.tar.gz
+cd ImageMagick-7.1.0-30
+
+./configure --prefix=$1 
+
+make -j
+make install
+
