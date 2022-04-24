7305fa76b95cd7d2a756f736030e82066883cecc
Qiyu8 <fangchunlin@huawei.com>
2022-04-24 09:39:44 +0800

    The dependcy should download inside the script

--- package/fftw/3.3.10/install.sh
+++ package/fftw/3.3.10/install.sh
@@ -3,6 +3,7 @@ set -x
 set -e
 cd ${JARVIS_TMP}
 rm -rf fftw-3.3.10
+${DOWNLOAD_TOOL} -u http://www.fftw.org/fftw-3.3.10.tar.gz
 tar -xvf ${JARVIS_DOWNLOAD}/fftw-3.3.10.tar.gz
 cd fftw-3.3.10
 ./configure --prefix=$1 MPICC=mpicc --enable-shared --enable-threads --enable-openmp --enable-mpi
