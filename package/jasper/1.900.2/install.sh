b89280b97549404b0fcff7a5327113fe564a3bae
wanghanwen <1564586847@qq.com>
2022-04-25 18:12:47 +0800

    修改部分脚本，新增relion、opencv、gat及其依赖脚本

--- package/jasper/1.900.2/install.sh
+++ package/jasper/1.900.2/install.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 set -x
 set -e
-
+. ${DOWNLOAD_TOOL} -u https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.2.tar.gz
 cd ${JARVIS_TMP}
 rm -rf jasper-1.900.2
 tar -xvf ${JARVIS_DOWNLOAD}/jasper-1.900.2.tar.gz
 cd jasper-1.900.2
-#CC=clang CXX=clang++ 
+#export CC=clang CXX=clang++ 
 ./configure --prefix=$1
 make -j
 make install
