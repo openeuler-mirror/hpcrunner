acd3c60a9c39aab352d55d7208603cd3e3403cbe
ye-jianbang <yejianbang1@h-partners.com>
2022-05-19 11:09:51 +0800

    add openblas_install.sh

---
+++ package/openblas/0.3.18/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+set -e
+. ${DOWNLOAD_TOOL} -u https://github.com/xianyi/OpenBLAS/archive/refs/tags/v0.3.18.tar.gz -f OpenBLAS-0.3.18.tar.gz
+cd ${JARVIS_TMP}
+tar -xzvf ${JARVIS_DOWNLOAD}/OpenBLAS-0.3.18.tar.gz
+cd OpenBLAS-0.3.18
+make -j 
+make PREFIX=$1 install
