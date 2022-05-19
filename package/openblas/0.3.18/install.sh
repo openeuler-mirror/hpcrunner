a566c979442454967e7df256e099548b064ceeeb
yejianbang <yejianbang1@h-partners.com>
2022-05-19 02:28:08 +0000

    删除文件 package/openblas/0.3.18/install.sh

--- package/openblas/0.3.18/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-. ${DOWNLOAD_TOOL} -u https://github.com/xianyi/OpenBLAS/releases/download/v0.3.18/OpenBLAS-0.3.18.tar.gz
-cd ${JARVIS_TMP}
-tar -xzvf ${JARVIS_DOWNLOAD}/OpenBLAS-0.3.18.tar.gz
-cd OpenBLAS-0.3.18
-make 
-make PREFIX=$1 install
