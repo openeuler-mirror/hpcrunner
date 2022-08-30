3e42084226f87f732c9b37c14256a6fccfb262b4
Qiyu8 <fangchunlin@huawei.com>
2022-08-30 10:03:42 +0800

    update package netcdf and pnetcdf

--- package/netcdf/4.7.4/install.sh
+++ package/netcdf/4.7.4/install.sh
@@ -2,13 +2,15 @@
 
 set -x
 set -e
-. ${DOWNLOAD_TOOL} -u https://codeload.github.com/Unidata/netcdf-fortran/tar.gz/refs/tags/v4.5.3 -f netcdf-fortran-4.5.3.tar.gz
-. ${DOWNLOAD_TOOL} -u https://codeload.github.com/Unidata/netcdf-c/tar.gz/refs/tags/v4.7.4 -f netcdf-c-4.7.4.tar.gz 
+netcdf_c_version='4.7.4'
+netcdf_f_version='4.5.3'
+. ${DOWNLOAD_TOOL} -u https://codeload.github.com/Unidata/netcdf-fortran/tar.gz/refs/tags/v${netcdf_f_version} -f netcdf-fortran-${netcdf_f_version}.tar.gz
+. ${DOWNLOAD_TOOL} -u https://codeload.github.com/Unidata/netcdf-c/tar.gz/refs/tags/v${netcdf_c_version} -f netcdf-c-${netcdf_c_version}.tar.gz 
 cd ${JARVIS_TMP}
-rm -rf netcdf-c-4.7.4 netcdf-fortran-4.5.3
-tar -xvf ${JARVIS_DOWNLOAD}/netcdf-c-4.7.4.tar.gz
-tar -xvf ${JARVIS_DOWNLOAD}/netcdf-fortran-4.5.3.tar.gz
-cd netcdf-c-4.7.4
+rm -rf netcdf-c-${netcdf_c_version} netcdf-fortran-${netcdf_f_version}
+tar -xvf ${JARVIS_DOWNLOAD}/netcdf-c-${netcdf_c_version}.tar.gz
+tar -xvf ${JARVIS_DOWNLOAD}/netcdf-fortran-${netcdf_f_version}.tar.gz
+cd netcdf-c-${netcdf_c_version}
 HDF5_DIR=${1%/*/*}/hdf5-clang/1.12.0
 PNETCDF_DIR=${1%/*/*}/pnetcdf/1.12.1
 ./configure --prefix=$1 --build=aarch64-unknown-linux-gnu --enable-shared --enable-netcdf-4 --disable-dap --with-pic --disable-doxygen --enable-static --enable-pnetcdf --enable-largefile CPPFLAGS="-O3 -I${HDF5_DIR}/include -I${PNETCDF_DIR}/include" LDFLAGS="-L${HDF5_DIR}/lib -L${PNETCDF_DIR}/lib -Wl,-rpath=${HDF5_DIR}/lib -Wl,-rpath=${PNETCDF_DIR}/lib" CFLAGS="-O3 -L${HDF5_DIR}/lib -L${PNETCDF_DIR}/lib -I${HDF5_DIR}/include -I${PNETCDF_DIR}/include"
@@ -20,7 +22,7 @@ export PATH=$1/bin:$PATH
 export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
 export NETCDF=${1}
 
-cd ../netcdf-fortran-4.5.3
+cd ../netcdf-fortran-${netcdf_f_version}
 ./configure --prefix=$1 --build=aarch64-unknown-linux-gnu --enable-shared --with-pic --disable-doxygen --enable-largefile --enable-static CPPFLAGS="-O3 -I${HDF5_DIR}/include -I${1}/include" LDFLAGS="-L${HDF5_DIR}/lib -L${1}/lib -Wl,-rpath=${HDF5_DIR}/lib -Wl,-rpath=${1}/lib" CFLAGS="-O3 -L${HDF5_DIR}/HDF5/lib -L${1}/lib -I${HDF5_DIR}/include -I${1}/include" CXXFLAGS="-O3 -L${HDF5_DIR}/lib -L${1}/lib -I${HDF5_DIR}/include -I${1}/include" FCFLAGS="-O3 -L${HDF5_DIR}/lib -L${1}/lib -I${HDF5_DIR}/include -I${1}/include"
 sed -i '11838c wl="-Wl,"' libtool
 make -j16
--- package/pnetcdf/1.12.1/install.sh
+++ package/pnetcdf/1.12.1/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 set -e
-. ${DOWNLOAD_TOOL} -u http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/pnetcdf-1.12.1.tar.gz
+pnetcdf_version='1.12.1'
+. ${DOWNLOAD_TOOL} -u http://cucis.ece.northwestern.edu/projects/PnetCDF/Release/pnetcdf-${pnetcdf_version}.tar.gz
 cd ${JARVIS_TMP}
-tar zxvf ${JARVIS_DOWNLOAD}/pnetcdf-1.12.1.tar.gz
-cd pnetcdf-1.12.1
+tar zxvf ${JARVIS_DOWNLOAD}/pnetcdf-${pnetcdf_version}.tar.gz
+cd pnetcdf-${pnetcdf_version}
 ./configure --prefix=$1 --build=aarch64-linux --enable-shared --enable-fortran --enable-large-file-test
 make -j16
 make install
