14aefcca7d9d71d3294ca4d7e575fce29a581bcd
binshuo zu <binshuo@isrc.iscas.ac.cn>
2022-09-06 22:22:53 +0800

    fix: failed install boost amd version

--- package/boost/1.72.0/install.sh
+++ package/boost/1.72.0/install.sh
@@ -6,5 +6,6 @@ cd ${JARVIS_TMP}
 rm -rf boost_1_72_0
 tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.gz
 cd boost_1_72_0
+sed -i '60s/.*/#ifdef PTHREAD_STACK_MIN/' ./boost/thread/pthread/thread_data.hpp
 ./bootstrap.sh
 ./b2 install --prefix=$1
