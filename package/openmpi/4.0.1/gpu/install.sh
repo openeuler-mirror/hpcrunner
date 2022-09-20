96c9d52597f1179679bdb5a575f7dc7aa3ddde0c
chunlin <fangchunlin@huawei.com>
2022-09-20 02:52:25 +0000

    update package/openmpi/4.0.1/gpu/install.sh.
    Signed-off-by: chunlin <fangchunlin@huawei.com>

--- package/openmpi/4.0.1/gpu/install.sh
+++ package/openmpi/4.0.1/gpu/install.sh
@@ -10,7 +10,7 @@ openmpi_ver='4.0.1'
 #install GDRCOPY
 . ${DOWNLOAD_TOOL} -u https://github.com/NVIDIA/gdrcopy/archive/v${gdr_ver}.tar.gz
 . ${DOWNLOAD_TOOL} -u https://github.com/openucx/ucx/releases/download/v${ucx_version}/ucx-${ucx_version}.tar.gz
-. ${DOWNLOAD_TOOL} -u https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-${openmpi_ver}.tar.gz
+. ${DOWNLOAD_TOOL} -u https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-${openmpi_ver}.tar.gz
 tar -x -f ${JARVIS_DOWNLOAD}/v${gdr_ver}.tar.gz
 mkdir -p $1/gdrcopy/include $1/gdrcopy/lib64
 cd gdrcopy-${gdr_ver}
