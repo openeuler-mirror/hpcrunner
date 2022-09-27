6253cdff9d4ff8de2f11b8b485fcd9b63678f873
Season111 <zhougonghai666@gmail.com>
2022-09-27 16:50:36 +0800

    将测试样例安装至casedir中

--- package/elmer/test/install.sh
+++ package/elmer/test/install.sh
@@ -1,6 +1,6 @@
 set -x
 set -e
 . ${DOWNLOAD_TOOL} -u https://sources.debian.org/data/non-free/e/elmer-doc/2014.02.06-1~bpo70+1/ElmerTutorialsFiles_nonGUI.tar.gz
-cd ${JARVIS_TMP}
+cd ${JARVIS_TMP}/elmerfem-scc20
 rm -rf tutorials_files
 tar -xvf ${JARVIS_DOWNLOAD}/ElmerTutorialsFiles_nonGUI.tar.gz
