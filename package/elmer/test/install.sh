5af56a91446451370f7aac71660c2b6b369cbaa2
Season111 <zhougonghai666@gmail.com>
2022-09-28 20:18:13 +0800

    删除package目录下elmer安装脚本，修复安装过程

--- package/elmer/test/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-. ${DOWNLOAD_TOOL} -u https://sources.debian.org/data/non-free/e/elmer-doc/2014.02.06-1~bpo70+1/ElmerTutorialsFiles_nonGUI.tar.gz
-cd ${JARVIS_TMP}/elmerfem-scc20
-rm -rf tutorials_files
-tar -xvf ${JARVIS_DOWNLOAD}/ElmerTutorialsFiles_nonGUI.tar.gz
