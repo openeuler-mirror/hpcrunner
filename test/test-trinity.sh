bec4a55802788b1056085b518bbee9a17395338d
hamster <hujing@isrc.iscas.ac.cn>
2022-10-12 06:31:23 +0000

    update test/test-trinity.sh.
    Signed-off-by: hamster <hujing@isrc.iscas.ac.cn>

--- test/test-trinity.sh
+++ test/test-trinity.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ..
 # release bowtie2 src code
-rm -rf tmp/bowtie2-2.4.5
+rm -rf tmp/trinityrnaseq-v2.14.0.FULL_with_extendedTestData
 # copy templates
-cp -rf templates/bowtie2/2.4.5/data.trinity.x86.cpu.config ./
+cp -rf templates/trinity/2.14.0/data.trinity.x86.cpu.config ./
 # switch to config
 ./jarvis -use data.trinity.x86.cpu.config
 # download bowtie2 src code
