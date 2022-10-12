b8259fb360a9f59788fc9fc0271ddd82b88c6e23
hamster <hujing@isrc.iscas.ac.cn>
2022-10-12 10:21:22 +0000

    update package/salmon/1.9.0/bisheng/install.sh.
    Signed-off-by: hamster <hujing@isrc.iscas.ac.cn>

--- package/salmon/1.9.0/bisheng/install.sh
+++ package/salmon/1.9.0/bisheng/install.sh
@@ -7,8 +7,4 @@ tar -xvf ${JARVIS_DOWNLOAD}/v1.9.0.tar.gz
 cd salmon-1.9.0
 cmake -DNO_IPO=TRUE -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_FLAGS="-O3 -march=armv8.2-a -mtune=tsv110" -DCMAKE_CXX_FLAGS="-O3 -march=armv8.2-a -mtune=tsv110 -stdlib=libc++" -DCMAKE_EXE_LINKER_FLAGS="-stdlib=libc++ -lc++ -lc++abi" -DCMAKE_INSTALL_PREFIX=$1 .
 make
-mkdir -p $1/bin $1/lib
-cp src/salmon $1/bin
-cp -r external/install/lib/* $1/lib
-cp external/install/lib64/lib* $1/lib
-
+make install
