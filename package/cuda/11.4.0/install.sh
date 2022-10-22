4b933f2c6a81e5ed0408cb217cd9c64d4ecafec3
chunlin <fangchunlin@huawei.com>
2022-10-22 06:47:38 +0000

    update package/cuda/11.4.0/install.sh.
    Signed-off-by: chunlin <fangchunlin@huawei.com>

--- package/cuda/11.4.0/install.sh
+++ package/cuda/11.4.0/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x
 set -e
-. ${DOWNLOAD_TOOL} -u https://developer.download.nvidia.com/compute/cuda/11.4.0/local_installers/cuda_11.4.0_470.42.01_linux_sbsa.run
+cuda_ver='11.4.4_470.82.01'
+. ${DOWNLOAD_TOOL} -u https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda_${cuda_ver}_linux_sbsa.run
 #禁用nouveau驱动
 mv /etc/modprobe.d/disable-nouveau.conf /etc/modprobe.d/disable-nouveau.conf.bak
 echo -e 'blacklist nouveau\noptions nouveau modeset=0' >/etc/modprobe.d/disable-nouveau.conf
 cp /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nouveau.img
 dracut -f /boot/initramfs-$(uname -r).img $(uname -r)
 
-sh $JARVIS_DOWNLOAD/cuda_11.4.0_470.42.01_linux_sbsa.run
+sh $JARVIS_DOWNLOAD/cuda_${cuda_ver}_linux_sbsa.run
