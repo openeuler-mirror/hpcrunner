f1664135f8bc745db2b1009ed0100bbaa36df0a5
Qiyu8 <fangchunlin@huawei.com>
2022-12-12 19:37:45 +0800

    add bisheng new version

--- package/bisheng/2.3.0/install.sh
+++ package/bisheng/2.3.0/install.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-set -e
-bisheng_version='2.3.0'
-arch='x86'
-if [ x$(arch) = xaarch64 ];then
-	arch='aarch64'
-fi
-. ${DOWNLOAD_TOOL} -u https://mirrors.huaweicloud.com/kunpeng/archive/compiler/bisheng_compiler/bisheng-compiler-${bisheng_version}-${arch}-linux.tar.gz
-cd ${JARVIS_TMP}
-. $CHECK_ROOT && yum -y install libatomic libstdc++ libstdc++-devel
-tar xzvf ${JARVIS_DOWNLOAD}/bisheng-compiler-${bisheng_version}-${arch}-linux.tar.gz -C $1 --strip-components=1
+bisheng_ver='bisheng-compiler-2.3.0'
+../meta.sh $1
---
+++ package/bisheng/2.4.0/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+export bisheng_ver='BiSheng-compiler-2.4.0'
+../meta.sh $1
