cc82bfa60814c24e754b3230b2b25999eda755c2
Qiyu8 <fangchunlin@huawei.com>
2023-02-11 08:10:02 +0000

    添加package/zlib/1.2.11
    Signed-off-by: chenshaoheng <12344440+chenshaoheng@user.noreply.gitee.com>

---
+++ package/zlib/1.2.11/clang/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+set -e
+zlib_ver='1.2.11'
+. ${DOWNLOAD_TOOL} -u https://zlib.net/fossils/zlib-${zlib_ver}.tar.gz
+cd ${JARVIS_TMP}
+rm -rf zlib-${zlib_ver}
+tar -xvf ${JARVIS_DOWNLOAD}/zlib-${zlib_ver}.tar.gz
+cd zlib-${zlib_ver}
+./configure --prefix=$1 FCFLAGS="-O3 -fPIC"
+make -j
+make install
