f64504da6d844742825ad26945f93eb4c4470765
chenshaoheng <chenshaoheng3@h-partners.com>
2023-02-14 01:56:34 +0000

    update package/libjpeg/v9b/install.sh.
    Signed-off-by: chenshaoheng <chenshaoheng3@h-partners.com>

--- package/libjpeg/v9b/install.sh
+++ package/libjpeg/v9b/install.sh
@@ -3,6 +3,7 @@ set -x
 set -e
 . ${DOWNLOAD_TOOL} -u http://www.ijg.org/files/jpegsrc.v9b.tar.gz
 cd ${JARVIS_TMP}
+rm -rf jpeg-9b
 tar xvf ${JARVIS_DOWNLOAD}/jpegsrc.v9b.tar.gz
 cd jpeg-9b
 ./configure --prefix=$1 --build=aarch64-unknown-linux-gnu
