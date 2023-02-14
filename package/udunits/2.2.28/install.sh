8a53569c134d508390c0da147658ead02366d4fb
chenshaoheng <chenshaoheng3@h-partners.com>
2023-02-14 09:43:26 +0000

    update package/udunits/2.2.28/install.sh.
    Signed-off-by: chenshaoheng <chenshaoheng3@h-partners.com>

--- package/udunits/2.2.28/install.sh
+++ package/udunits/2.2.28/install.sh
@@ -3,6 +3,7 @@ set -x
 set -e
 . ${DOWNLOAD_TOOL} -u https://artifacts.unidata.ucar.edu/repository/downloads-udunits/2.2.28/udunits-2.2.28.tar.gz
 cd ${JARVIS_TMP}
+rm -rf udunits-2.2.28
 tar xvf ${JARVIS_DOWNLOAD}/udunits-2.2.28.tar.gz
 cd udunits-2.2.28
 ./configure --prefix=$1
