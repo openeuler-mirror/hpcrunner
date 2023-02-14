0e2cee7f696017b79748f077853b2ec77fbce606
chenshaoheng <chenshaoheng3@h-partners.com>
2023-02-14 01:58:41 +0000

    update package/gdal/2.2.4/install.sh.
    Signed-off-by: chenshaoheng <chenshaoheng3@h-partners.com>

--- package/gdal/2.2.4/install.sh
+++ package/gdal/2.2.4/install.sh
@@ -9,7 +9,7 @@ cd gdal-2.2.4
 ./configure --with-static-proj4=${PROJ_PATH} --prefix=$1 --with-png=${LIBPNG_PATH} --with-gif=internal --with-libtiff=internal \
       --with-geotiff=internal --with-jpeg=${LIBJPEG_PATH} --with-libz=/usr/local \
       --with-sqlite3=no --with-expat=no --with-curl=no --without-ld-shared \
-      --with-hdf4=no --with-hdf5=no --with-pg=no --without-grib --disable-shared \
+      --with-hdf4=no --with-hdf5=no --with-pg=no --without-grib --enable-shared \
       --with-freexl=no --with-geos=no --with-openjpeg=no --with-mysql=no \
       --with-ecw=no --with-fgdb=no --with-odbc=no --with-xml2=no --with-ogdi=no\
       --with-pcraster=no --with-xerces=no
