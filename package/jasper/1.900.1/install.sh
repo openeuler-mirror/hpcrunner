91ec53ff7f27a54e952c65ed748591da8e25c93a
Qiyu8 <fangchunlin@huawei.com>
2023-03-01 11:06:18 +0800

    fix install

--- package/jasper/1.900.1/install.sh
+++ package/jasper/1.900.1/install.sh
@@ -7,7 +7,6 @@ cd ${JARVIS_TMP}
 rm -rf jasper-1.900.2
 tar -xvf ${JARVIS_DOWNLOAD}/jasper-1.900.2.tar.gz
 cd jasper-1.900.2
-#export CC=clang CXX=clang++ 
 ./configure --prefix=$1
 make -j
 make install
