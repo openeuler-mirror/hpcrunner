8ca4b47fff662ac3f199041074b262e02a3a7176
chenshaoheng <chenshaoheng3@h-partners.com>
2023-03-03 10:25:53 +0800

    fix hdf51.10.1 install

--- package/hdf5/1.10.1/install.sh
+++ package/hdf5/1.10.1/install.sh
@@ -9,5 +9,8 @@ tar -xvf ${JARVIS_DOWNLOAD}/hdf5-1.10.1.tar.gz
 cd hdf5-1.10.1
 #CC=mpicc CXX=mpicxx FC=mpifort F77=mpifort -Wno-incompatible-pointer-types-discards-qualifiers
 ./configure --prefix=$1  --enable-fortran --enable-static=yes --enable-parallel --enable-shared
+sed -i '11835c wl=-Wl," libtool
+sed
+sed -i 11835c
 make -j
 make install
