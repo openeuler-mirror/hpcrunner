55d6b63f55f3928286024ae2de76fcb974ce46c9
chenshaoheng <chenshaoheng3@h-partners.com>
2023-03-13 17:46:36 +0800

    fix package/netcdf/4.7.0/clang/install.sh

--- package/netcdf/4.7.0/clang/install.sh
+++ package/netcdf/4.7.0/clang/install.sh
@@ -19,6 +19,13 @@ fi
 export CC=mpicc CXX=mpicxx FC=mpifort
 HDF5_DIR=${HDF5_CLANG_PATH}
 PNETCDF_DIR=${PNETCDF_PATH}
+
+cd include/
+rm -rf ncconfigure.h
+wget https://github.com/Unidata/netcdf-c/files/3234181/ncconfigure.zip --no-ch
+unzip ncconfigure.zip
+cd ..
+
 ./configure --prefix=$1 ${build_type} --enable-shared --enable-netcdf-4 --disable-dap --with-pic --disable-doxygen --enable-static --enable-pnetcdf --enable-largefile CPPFLAGS="-O3 -I${HDF5_DIR}/include -I${PNETCDF_DIR}/include" LDFLAGS="-L${HDF5_DIR}/lib -L${PNETCDF_DIR}/lib -Wl,-rpath=${HDF5_DIR}/lib -Wl,-rpath=${PNETCDF_DIR}/lib" CFLAGS="-O3 -L${HDF5_DIR}/lib -L${PNETCDF_DIR}/lib -I${HDF5_DIR}/include -I${PNETCDF_DIR}/include"
 
 make -j16
