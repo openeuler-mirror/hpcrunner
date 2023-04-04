e1e180c64e5808b40adfe44ace9cfbd94dafa228
Qiyu8 <fangchunlin@huawei.com>
2023-04-04 15:04:56 +0800

    add gromacs 2022.5 config

--- package/kfft/1.0.0/install.sh
+++ package/kfft/1.0.0/install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -x
 set -e
-lib_name='kml_adapter'
-. ${DOWNLOAD_TOOL} -u https://gitee.com/openeuler/kml_adapter.git -t git
+lib_name='kml_adapter-master'
+#. ${DOWNLOAD_TOOL} -u https://gitee.com/openeuler/kml_adapter.git -t git
 cd $JARVIS_DOWNLOAD
 cd ./$lib_name/kml_fft_adapter
-export C_INCLUDE_PATH=/usr/local/kml/include/
-export CPLUS_INCLUDE_PATH=/usr/local/kml/include/
+sed -i "s/\/usr\/local\/kml/\$ENV{KML_GCC_PATH}/g" CMakeLists.txt
+export C_INCLUDE_PATH=$KML_GCC_PATH/include/
+export CPLUS_INCLUDE_PATH=$KML_GCC_PATH/include/
 chmod +x build.sh
 dos2unix build.sh
 rm -rf build
 ./build.sh
 mkdir -p $1/lib
 cp -rf build/* $1/lib
+cp -rf include $1/
