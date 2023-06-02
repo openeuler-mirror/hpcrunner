120bf6c91d5cc2896ad4cd2804a471931d8402f5
yuanchunlin <yuanchunlin2@h-partners.com>
2023-06-02 09:14:12 +0000

    update package/fftw/3.3.10/install.sh.
    Signed-off-by: yuanchunlin <yuanchunlin2@h-partners.com>

--- package/fftw/3.3.10/install.sh
+++ package/fftw/3.3.10/install.sh
@@ -6,5 +6,11 @@ rm -rf fftw-3.3.10
 ${DOWNLOAD_TOOL} -u http://www.fftw.org/fftw-3.3.10.tar.gz
 tar -xvf ${JARVIS_DOWNLOAD}/fftw-3.3.10.tar.gz
 cd fftw-3.3.10
-./configure --prefix=$1 MPICC=mpicc --enable-shared --enable-threads --enable-openmp --enable-mpi
-make -j install
+./configure --prefix=$1 --enable-single --enable-float --enable-neon --enable-shared --enable-threads --enable-openmp --enable-mpi CFLAGS="-O3 -fomit-frame-pointer -fstrict-aliasing"
+make -j && make install
+make clean
+./configure --prefix=$1 --enable-long-double --enable-shared --enable-threads --enable-openmp --enable-mpi CFLAGS="-O3 -fomit-frame-pointer -fstrict-aliasing"
+make -j && make install
+make clean
+./configure --prefix=$1 --enable-shared --enable-threads --enable-openmp --enable-mpi CFLAGS="-O3 -fomit-frame-pointer -fstrict-aliasing"
+make -j && make install
