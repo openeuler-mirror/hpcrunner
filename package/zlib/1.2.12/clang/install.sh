14794a507fbbf8507f820945cb8c19b7ee566051
chenshaoheng <chenshaoheng3@h-partners.com>
2023-08-01 02:04:02 +0000

    update package/zlib/1.2.12/clang/install.sh.
    Signed-off-by: chenshaoheng <chenshaoheng3@h-partners.com>

--- package/zlib/1.2.12/clang/install.sh
+++ package/zlib/1.2.12/clang/install.sh
@@ -4,8 +4,10 @@ set -e
 zlib_ver='1.2.12'
 . ${DOWNLOAD_TOOL} -u https://zlib.net/fossils/zlib-${zlib_ver}.tar.gz
 cd ${JARVIS_TMP}
+rm -rf zlib-${zlib_ver}
+export FFLAGS="-O3 -fPIC" CFLAGS="-O3 -fPIC" CXXFLAGS="-O3 -fPIC"
 tar -xvf ${JARVIS_DOWNLOAD}/zlib-${zlib_ver}.tar.gz
 cd zlib-${zlib_ver}
-./configure --prefix=$1 FCFLAGS="-O3 -fPIC"
+./configure --prefix=$1 
 make -j
 make install
