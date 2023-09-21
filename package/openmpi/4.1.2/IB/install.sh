df5161a42c819fbc08f05fd9a29737ae84198bfa
iotwins <fangchunlin@huawei.com>
2023-09-21 19:39:09 +0800

    1.fix openmpi no ucx error 2.fix openmpi/hmpi recognize bug 3.add opensource stack config

--- package/openmpi/4.1.2/IB/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -x
-set -e
-. ${DOWNLOAD_TOOL} -u https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.2.tar.gz
-cd ${JARVIS_TMP}
-tar -xvf ${JARVIS_DOWNLOAD}/openmpi-4.1.2.tar.gz
-cd openmpi-4.1.2
-./configure CC=gcc CXX=g++ FC=gfortran --prefix=$1 --enable-pretty-print-stacktrace --enable-orterun-prefix-by-default --with-cma --with-ucx --enable-mpi1-compatibility
-make -j install
