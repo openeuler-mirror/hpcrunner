b90ea133ead1eef414f106eb6864cdaca38a29cd
chenshaoheng <chenshaoheng3@h-partners.com>
2023-10-16 03:55:35 +0000

    update package/lapack/3.10.1/bisheng/install.sh.Support compiling dynamic libraries
    Signed-off-by: chenshaoheng <chenshaoheng3@h-partners.com>

--- package/lapack/3.10.1/bisheng/install.sh
+++ package/lapack/3.10.1/bisheng/install.sh
@@ -5,7 +5,6 @@ set -e
 cd ${JARVIS_TMP}
 tar -xvf ${JARVIS_DOWNLOAD}/lapack-3.10.1.tar.gz
 cd lapack-3.10.1
-cp make.inc.example make.inc
 sed -i '9s/gcc/clang/g' ./make.inc
 sed -i '10s/$/& -march=native/g' ./make.inc
 sed -i '20s/gfortran/flang/g' ./make.inc
@@ -14,7 +13,10 @@ sed -i '23s/-frecursive$//g' ./make.inc
 sed -i '30s/gfortran/flang/g' ./make.inc
 sed -i '46s/^/#  /g' ./make.inc
 sed -i '50s/^#//g' ./make.inc
+rm build -rf
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=$1 -DBUILD_SHARED_LIBS=ON
 make -j $(nproc)
-mkdir $1/lib/
-cp *.a $1/lib/
-cp -r LAPACKE/include $1/
+make install
+ln -s $1/lib64 $1/lib
