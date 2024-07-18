bd6c7cdcbd5c45832c50d6302c30d291ddf57067
chenshaoheng <chenshaoheng3@h-partners.com>
2024-07-18 11:50:37 +0800

    fix amg/1.2

--- package/amg/1.2/install.sh
+++ package/amg/1.2/install.sh
@@ -7,7 +7,7 @@ cd ${JARVIS_ROOT}
 rm -rf AMG-${amg_ver}
 unzip ${JARVIS_DOWNLOAD}/${amg_ver}.zip
 cd AMG-${amg_ver} 
-sed -i "46c INCLUDE_CFLAGS = -O2 -std=c11 -DTIMER_USE_MPI -DHYPRE_USING_OPENMP -fopenmp -DHYPRE_HOPSCOTCH -DHYPRE_USING_PERSISTENT_COMM -DHYPRE_BIGINT -mcpu=linxicore9100  -mllvm -force-customized-pipeline=true" Makefile.include
+sed -i "46c INCLUDE_CFLAGS = -O2 -std=c89 -DTIMER_USE_MPI -DHYPRE_USING_OPENMP -fopenmp -DHYPRE_HOPSCOTCH -DHYPRE_USING_PERSISTENT_COMM -DHYPRE_BIGINT -mcpu=linxicore9100  -mllvm -force-customized-pipeline=true" Makefile.include
 make -j
 mkdir $1/bin -p
 cp ./test/* $1/bin
