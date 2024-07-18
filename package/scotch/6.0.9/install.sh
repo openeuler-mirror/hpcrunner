48015ca05a6a69f057cb9021937a02c0f4572bc8
chenshaoheng <chenshaoheng3@h-partners.com>
2024-07-18 20:13:22 +0800

    fix package/scotch/6.0.9/install.sh

--- package/scotch/6.0.9/install.sh
+++ package/scotch/6.0.9/install.sh
@@ -7,9 +7,6 @@ rm  -rf scotch_6.0.9
 tar -xvf ${JARVIS_DOWNLOAD}/scotch_6.0.9.tar.gz
 cd scotch_6.0.9
 cd src
-sed -i "6c AR              = clang" ./Make.inc/Makefile.inc.x86-64_pc_linux2.shlib
-sed -i "9c CCS             = clang" ./Make.inc/Makefile.inc.x86-64_pc_linux2.shlib
-sed -i "11c CCD             = clang" ./Make.inc/Makefile.inc.x86-64_pc_linux2.shlib
 ln -s ./Make.inc/Makefile.inc.x86-64_pc_linux2.shlib Makefile.inc
 make scotch
 mkdir $1/bin -p
