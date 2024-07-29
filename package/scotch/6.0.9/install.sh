d959de06012d3e28d1eba0e9eb1ba932b7aed84e
chenshaoheng <chenshaoheng3@h-partners.com>
2024-07-29 10:13:13 +0800

    add package/scotch/6.0.9/install.sh

--- package/scotch/6.0.9/install.sh
+++ package/scotch/6.0.9/install.sh
@@ -11,5 +11,7 @@ ln -s ./Make.inc/Makefile.inc.x86-64_pc_linux2.shlib Makefile.inc
 make scotch
 mkdir $1/bin -p
 mkdir $1/lib -p
+mkdir $1/include -p
 cp ../bin/* $1/bin
 cp ../lib/* $1/lib
+cp ../include/* $1/include
