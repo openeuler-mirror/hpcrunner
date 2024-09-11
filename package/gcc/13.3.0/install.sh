db00072d4c904e8dcb6879ea84c4567c092ad25b
Caohongtao <caohongtao_yewu@cmss.chinamobile.com>
2024-09-11 09:28:01 +0000

    Add package/gcc-13.3.0
    Signed-off-by: Caohongtao <caohongtao_yewu@cmss.chinamobile.com>

---
+++ package/gcc/13.3.0/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+set -e
+. ${DOWNLOAD_TOOL} -u https://ftp.gnu.org/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.gz
+cd ${JARVIS_TMP}
+rm -rf gcc-13.3.0
+tar -xzvf ${JARVIS_DOWNLOAD}/gcc-13.3.0.tar.gz
+cd gcc-10.3.0
+./contrib/download_prerequisites
+./configure --disable-multilib --enable-languages="c,c++,fortran" --prefix=$1 --disable-static --enable-shared
+make -j && make install
