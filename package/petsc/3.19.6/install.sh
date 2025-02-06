2cc8f38abf6bd2c20d68c5bfba710e0bb7f4ca6e
limingyang <limingyang49@huawei.com>
2025-02-06 09:37:58 +0000

    update package/petsc/3.19.6/install.sh.
    Signed-off-by: limingyang <limingyang49@huawei.com>

--- package/petsc/3.19.6/install.sh
+++ package/petsc/3.19.6/install.sh
@@ -8,6 +8,6 @@ tar -xvf ${JARVIS_DOWNLOAD}/petsc-3.19.6.tar.gz
 cd petsc-3.19.6
 ./configure F77=$F77 --prefix=$1 --with-cc=mpicc --with-cxx=mpicxx --with-fc=mpifort \
 	--download-parmetis=1 --download-metis=1 \
-	--download-hdf5=${JARVIS_DOWNLOAD}/hdf5-1.13.0.tar.bz2 --force
+	--download-hdf5=${JARVIS_DOWNLOAD} --force
 make PETSC_DIR=${JARVIS_TMP}/petsc-3.19.6 PETSC_ARCH=arch-linux-c-debug all -j
 make PETSC_DIR=${JARVIS_TMP}/petsc-3.19.6 PETSC_ARCH=arch-linux-c-debug install -j
