472ba76a1078e1056b9961ec3354bddb3cb2ecd6
limingyang <limingyang49@huawei.com>
2025-02-06 09:35:13 +0000

    update package/boost/1.72.0/clang/install.sh.
    Signed-off-by: limingyang <limingyang49@huawei.com>

--- package/boost/1.72.0/clang/install.sh
+++ package/boost/1.72.0/clang/install.sh
@@ -7,4 +7,4 @@ tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.gz
 cd boost_1_72_0
 sed -i '60s/.*/#ifdef PTHREAD_STACK_MIN/' ./boost/thread/pthread/thread_data.hpp
 ./bootstrap.sh --with-toolset=clang
-./b2 toolset=clang cxxflags="-stdlib=libc++" linkflags="-stdlib=libc++" install --prefix=$1
+./b2 toolset=clang cxxflags="-std=c++14 -stdlib=libc++ -Wno-error=enum-constexpr-conversion" linkflags="-std=c++14 -stdlib=libc++ -Wno-error=enum-constexpr-conversion" install --prefix=$1
