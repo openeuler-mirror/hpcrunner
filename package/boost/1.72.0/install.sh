974ea32035e70ad61dfec29103a019ac79155188
xiaolinjunhao <981755938@qq.com>
2025-02-17 08:51:03 +0000

    描述：新增应用cufflinks2.2.1，以及其依赖samtools1.10 用户：肖林俊浩x00806476

--- package/boost/1.72.0/install.sh
+++ package/boost/1.72.0/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -x
 set -e
-. ${DOWNLOAD_TOOL} -u https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz
+. ${DOWNLOAD_TOOL} -u https://archives.boost.io/release/1.72.0/source/boost_1_72_0.tar.gz
+#https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz
 cd ${JARVIS_TMP}
 rm -rf boost_1_72_0
 tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.gz
