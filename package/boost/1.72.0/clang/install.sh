d0ac14b4b22ef88ace3e5497262c34aa45c8f74c
chenshaoheng <chenshaoheng3@h-partners.com>
2025-03-05 18:05:02 +0800

    add templates/psi4/1.3.2/data.psi4-common.arm.cpu.config

--- package/boost/1.72.0/clang/install.sh
+++ package/boost/1.72.0/clang/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 set -e
-. ${DOWNLOAD_TOOL} -u https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.gz
+. ${DOWNLOAD_TOOL} -u https://nchc.dl.sourceforge.net/project/boost/boost/1.72.0/boost_1_72_0.tar.bz2
 cd ${JARVIS_TMP}
-tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.gz
+tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.bz2
 cd boost_1_72_0
 sed -i '60s/.*/#ifdef PTHREAD_STACK_MIN/' ./boost/thread/pthread/thread_data.hpp
 ./bootstrap.sh --with-toolset=clang --with-libraries=system,serialization,program_options
