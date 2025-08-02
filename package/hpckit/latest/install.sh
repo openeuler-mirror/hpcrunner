f3650c97cd85796f1fb6fd3f4f83dc304713977f
sunlock0653 <sherlock2006@163.com>
2025-08-02 14:21:31 +0800

    update netcdf-fortran

--- package/hpckit/latest/install.sh
+++ package/hpckit/latest/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-export hpckit_ver=`curl https://mirrors.huaweicloud.com/kunpeng/archive/HPC/HPCKit/| awk -F "HPCKit_" '{print $2}'|awk -F "_Linux-aarch64" '{print $1}'|awk '{lines[NR]=$0} END{print lines[NR-1]}'`
+export hpckit_ver=` curl https://mirrors.huaweicloud.com/kunpeng/archive/HPC/HPCKit/| awk -F "HPCKit_" '{print $2}'|grep -v "RC"|awk -F "_Linux-aarch64" '{print $1}'|awk '{lines[NR]=$0} END{print lines[NR-1]}'`
 ../meta.sh $1
