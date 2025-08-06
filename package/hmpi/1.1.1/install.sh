acc5b730cf563bbe2d55aabd70302c3db52f0ae5
sunlock0653 <sherlock2006@163.com>
2025-08-06 21:11:11 +0800

    fix hmpi

--- package/hmpi/1.1.1/install.sh
+++ package/hmpi/1.1.1/install.sh
@@ -5,17 +5,25 @@ hmpi_version='1.1.1'
 if [[ $UseGitee -eq 1 ]]; then
 . ${DOWNLOAD_TOOL} -u https://gitee.com/kp-hpc-mod/hpc-src/raw/master/hucx-${hmpi_version}-huawei.zip -f hucx-${hmpi_version}-huawei.zip
 . ${DOWNLOAD_TOOL} -u https://gitee.com/kp-hpc-mod/hpc-src/raw/master/xucg-${hmpi_version}-huawei.zip -f xucg-${hmpi_version}-huawei.zip
-. ${DOWNLOAD_TOOL} -u https://gitee.com/kp-hpc-mod/hpc-src/raw/master/hmpi-v${hmpi_version}.zip -f hmpi-${hmpi_version}-huawei.zip
+
+cd ${JARVIS_TMP}
+rm -rf hmpi-${hmpi_version}-huawei 
+git clone https://gitee.com/kp-hpc-mod/hmpi.git
+mv hmpi hmpi-${hmpi_version}-huawei
+cd ${hmpi_version}-huawei
+git checkout v${hmpi_version}-huawei
+
 else
 . ${DOWNLOAD_TOOL} -u $JARVIS_PROXY/kunpengcompute/hucx/archive/refs/tags/v${hmpi_version}-huawei.zip -f hucx-${hmpi_version}-huawei.zip
 . ${DOWNLOAD_TOOL} -u $JARVIS_PROXY/kunpengcompute/xucg/archive/refs/tags/v${hmpi_version}-huawei.zip -f xucg-${hmpi_version}-huawei.zip
 . ${DOWNLOAD_TOOL} -u $JARVIS_PROXY/kunpengcompute/hmpi/archive/refs/tags/v${hmpi_version}-huawei.zip -f hmpi-${hmpi_version}-huawei.zip
+rm -rf hmpi-${hmpi_version}-huawei 
 fi
 
 cd ${JARVIS_TMP}
 . $CHECK_ROOT && yum install -y perl-Data-Dumper autoconf automake libtool binutils flex
 
-rm -rf hmpi-v${hmpi_version} hucx-${hmpi_version}-huawei xucg-${hmpi_version}-huawei
+hucx-${hmpi_version}-huawei xucg-${hmpi_version}-huawei
 unzip ${JARVIS_DOWNLOAD}/hucx-${hmpi_version}-huawei.zip
 unzip ${JARVIS_DOWNLOAD}/xucg-${hmpi_version}-huawei.zip
 unzip ${JARVIS_DOWNLOAD}/hmpi-${hmpi_version}-huawei.zip
@@ -30,7 +38,7 @@ for file in `find . -name Makefile`;do sed -i "s/-implicit-function-declaration/
 make -j
 make install
 
-cd ../hmpi-v${hmpi_version}
+cd ../hmpi-${hmpi_version}-huawei
 ./autogen.pl
 ./configure --prefix=$1 --with-platform=contrib/platform/mellanox/optimized --enable-mpi1-compatibility --with-ucx=$1/hucx
 make -j
