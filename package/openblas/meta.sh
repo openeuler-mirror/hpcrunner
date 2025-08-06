229df859511ebb5b31494632bf85390668ec3b2b
sunlock0653 <sherlock2006@163.com>
2025-08-06 16:56:11 +0800

    add gitee proxy

--- package/openblas/meta.sh
+++ package/openblas/meta.sh
@@ -3,14 +3,20 @@ set -x
 set -e
 
 if [[ $UseGitee -eq 1 ]]; then
-. ${DOWNLOAD_TOOL} -u https://gitee.com/kp-hpc-mod/OpenBLAS/archive/refs/tags/v${openblas_ver}.tar.gz -f OpenBLAS-${openblas_ver}.tar.gz
+#. ${DOWNLOAD_TOOL} -u https://gitee.com/kp-hpc-mod/OpenBLAS/archive/refs/tags/v${openblas_ver}.tar.gz -f OpenBLAS-${openblas_ver}.tar.gz
+cd ${JARVIS_TMP}
+git clone https://gitee.com/kp-hpc-mod/OpenBLAS.git
+cd OpenBLAS
+git checkout v${openblas_ver}
+
 else
 . ${DOWNLOAD_TOOL} -u $JARVIS_PROXY/OpenMathLib/OpenBLAS/releases/download/v${openblas_ver}/OpenBLAS-v${openblas_ver}.tar.gz -f OpenBLAS-${openblas_ver}.tar.gz
-fi
-
 cd ${JARVIS_TMP}
 rm -rf OpenBLAS-${openblas_ver}
 tar -xzvf ${JARVIS_DOWNLOAD}/OpenBLAS-${openblas_ver}.tar.gz
 cd OpenBLAS-${openblas_ver}
+fi
+
+
 make -j
 make PREFIX=$1 install
