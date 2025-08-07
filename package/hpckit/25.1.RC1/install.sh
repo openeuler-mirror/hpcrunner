5b3fb9baf8128a322b45e80f2d4c60aa28d4e6a5
DFRJ <fujiangjie@huawei.com>
2025-08-07 02:55:32 +0000

    add package/hpckit/25.1.RC1/install.sh. 1、合入hpckit的modulefiles到hpcrunner 2、hmpi的modulefile新增CC CXX FC F90 F77等环境变量
    Signed-off-by: DFRJ <fujiangjie@huawei.com>

---
+++ package/hpckit/25.1.RC1/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+export hpckit_ver="25.1.RC1"
+../meta.sh $1
+
+ln -s $1/HPCKit/${hpckit_ver}/modulefiles $1/../../../modulefiles/hpckit${hpckit_ver}
+
+for hmpi_file in `find -L $1/HPCKit/${hpckit_ver}/modulefiles -name release |grep -v kupl`
+do
+    echo -e "\nsetenv CC mpicc \nsetenv CXX mpicxx \nsetenv FC mpifort \nsetenv F77 mpifort \nsetenv F90 mpifort " >> ${hmpi_file}
+done
