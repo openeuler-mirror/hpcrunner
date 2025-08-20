f8f6d962386945eda8a3394fcf70418b20da3e91
dingronghua <2162443403@qq.com>
2025-08-20 06:23:17 +0000

    update package/bisheng/4.1.0/install.sh. 原链接失效，更新
    Signed-off-by: dingronghua <2162443403@qq.com>

--- package/bisheng/4.1.0/install.sh
+++ package/bisheng/4.1.0/install.sh
@@ -6,7 +6,7 @@ if [ x$(arch) = xaarch64 ];then
         arch='aarch64'
 fi
 # . ${DOWNLOAD_TOOL} -u https://mirrors.huaweicloud.com/kunpeng/archive/compiler/bisheng_compiler/${bisheng_ver}-${arch}-linux-mini-hpc.tar.gz
-. ${DOWNLOAD_TOOL} -u https://kunpeng-repo.obs.cn-north-4.myhuaweicloud.com/BiSheng%20Enterprise/BiSheng%20Enterprise%20203.0.0/${bisheng_ver}-${arch}-linux.tar.gz
+. ${DOWNLOAD_TOOL} -u https://mirrors.huaweicloud.com/kunpeng/archive/compiler/bisheng_compiler/${bisheng_ver}-${arch}-linux.tar.gz
 cd ${JARVIS_TMP}
 . $CHECK_ROOT && yum -y install libatomic libstdc++ libstdc++-devel
 tar xzvf ${JARVIS_DOWNLOAD}/${bisheng_ver}-${arch}-linux.tar.gz -C $1 --strip-components=1
