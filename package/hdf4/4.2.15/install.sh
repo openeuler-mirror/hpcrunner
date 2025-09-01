649857162b28b49f52af275e681671e32e9a1e46
jsfhd <2162443403@qq.com>
2025-09-01 14:56:50 +0800

    使用系统JPEG库

--- package/hdf4/4.2.15/install.sh
+++ package/hdf4/4.2.15/install.sh
@@ -10,6 +10,6 @@ sed -i '974c #if defined(__linux__) && defined __x86_64__ && !( defined SUN) || 
 sed -i '23660a LIBS="$LIBS -ltirpc"' configure
 sed -i '23662c CPPFLAGS="$SYSCPPFLAGS -I/usr/include/tirpc"' configure
 export CC=mpicc CXX=mpicxx FC=mpifort
-./configure --prefix=$1 --enable-production --with-zlib=/usr/lib64 --enable-fortran --enable-hdf4-xdr --disable-shared --build=arm-linux  --with-jpeg=${LIBJPEG_PATH} --disable-netcdf CFLAGS="-fPIC -Wno-error=int-conversion" CXXFLAGS="-fPIC -Wno-error=int-conversion" FFLAGS="-fPIC -fallow-argument-mismatch -Wno-error=int-conversion" LDFLAGS="-L/usr/lib64 -ltirpc" CPPFLAGS="-I/usr/include/tirpc" --build=aarch64-unknown-linux-gnu
+./configure --prefix=$1 --enable-production --with-zlib=/usr/lib64 --enable-fortran --enable-hdf4-xdr --disable-shared --build=arm-linux  --with-jpeg=/usr/lib64 --disable-netcdf CFLAGS="-fPIC -Wno-error=int-conversion" CXXFLAGS="-fPIC -Wno-error=int-conversion" FFLAGS="-fPIC -fallow-argument-mismatch -Wno-error=int-conversion" LDFLAGS="-L/usr/lib64 -ltirpc" CPPFLAGS="-I/usr/include/tirpc" --build=aarch64-unknown-linux-gnu
 make -j
 make install
