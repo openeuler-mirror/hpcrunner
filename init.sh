c533667ea4ad96cacf22ae1c94e9dae03fdb885e
sunlock0653 <sherlock2006@163.com>
2025-11-10 10:53:27 +0800

    fix hpckit problem2

--- init.sh
+++ init.sh
@@ -43,6 +43,8 @@ fi
 #判断hpckit使用版本
 if [ ${UseLatest} -eq 0 ];then
     HPCKIT_VERSION=25.1.0
+    BISHENG_VERSION=4.1.0
+    HMPI_VERSION=25.1.0
 elif [ ${UseLatest} -eq 1 ];then
     HPCKIT_VERSION=latest
 else
@@ -50,8 +52,16 @@ else
     exit 1
 fi
 
-BISHENG_VERSION=`ls software/utils/hpckit/${HPCKit_Version}/HPCKit/${HPCKit_Version}/modulefiles/bisheng|grep compiler|awk -F "compiler" '{print $2}'`
-HMPI_VERSION=`ls software/utils/hpckit/${HPCKit_Version}/HPCKit/${HPCKit_Version}/modulefiles/bisheng|grep hmpi|awk -F "hmpi" '{print $2}'`
+#判断hpckit是否安装，更新配套版本
+file_path="software/utils/hpckit/${HPCKit_Version}/HPCKit/${HPCKit_Version}/modulefiles/bisheng"
+if [ -e "$file_path" ]; then
+    echo -e "你正在使用 $HPCKIT_VERSION 版本的 HPCKKit"
+    BISHENG_VERSION=`ls $$file_path|grep compiler|awk -F "compiler" '{print $2}'`
+    HMPI_VERSION=`ls $$file_path|grep hmpi|awk -F "hmpi" '{print $2}'`
+else
+    echo -e "INFO: 检测到 $HPCKIT_VERSION 版本的 HPCKKit 未进行安装，请继续执行\n"
+fi
+
 
 #Install modules
 if ! type module >/dev/null 2>&1;then
