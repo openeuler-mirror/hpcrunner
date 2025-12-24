d7b3ee4b076a52a54f1c1377efa3393ed8a29e7f
sunlock0653 <sherlock2006@163.com>
2025-12-24 16:32:12 +0800

    update

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ chmod -R +x ./test
 chmod +x ./*.sh
 chmod +x jarvis
 mkdir -p tmp
+
+#设置hpcrunner相关环境变量
 export JARVIS_ROOT=${CUR_PATH} #hpcrunner所在路径
 export JARVIS_COMPILER=${CUR_PATH}/software/compiler
 export JARVIS_MPI=${CUR_PATH}/software/mpi
@@ -20,11 +22,9 @@ export JARVIS_EXE=${CUR_PATH}/exe
 export JARVIS_PROXY=https://github.com #github网络代理，默认使用官方源
 export UseGitee=1 #下载源是否优先使用gitee，1代表优先使用
 export UseLatest=0 #依赖是否优先安装最新版，1代表优先使用
-
 export DOWNLOAD_TOOL=${CUR_PATH}/package/common/download.sh
 export CHECK_DEPS=${CUR_PATH}/package/common/check_deps.sh
 export CHECK_ROOT=${CUR_PATH}/package/common/check_root.sh
-
 export kp=neon
 ifsme=`lscpu|grep sme`
 ifsve=`lscpu|grep sve`
@@ -40,7 +40,7 @@ else
     echo "not in kunpeng architecture"
 fi
 
-#判断hpckit使用版本
+#设置依赖相关环境变量
 if [ ${UseLatest} -eq 0 ];then
     export HPCKIT_VERSION=25.1.0
     export BISHENG_VERSION=4.2.0.2
@@ -52,13 +52,9 @@ else
     exit 1
 fi
 
-#判断hpckit是否安装，更新配套版本
-file_path="${JARVIS_ROOT}/software/utils/hpckit/${HPCKIT_VERSION}/HPCKit/${HPCKIT_VERSION}/modulefiles/bisheng"
-if [ -e "$file_path" ]; then
-    echo -e "你正在使用 $HPCKIT_VERSION 版本的 HPCKit"
+function load_hpckit() {
     export BISHENG_VERSION=`ls $file_path|grep compiler|awk -F "compiler" '{print $2}'`
     export HMPI_VERSION=`ls $file_path|grep hmpi|awk -F "hmpi" '{print $2}'`
-    #module purge
     module use ${JARVIS_ROOT}/software/utils/hpckit/${HPCKIT_VERSION}/HPCKit/${HPCKIT_VERSION}/modulefiles
     module load bisheng/compiler${BISHENG_VERSION}/bishengmodule
     module load bisheng/hmpi${HMPI_VERSION}/release
@@ -68,11 +64,62 @@ if [ -e "$file_path" ]; then
     export KML_PATH=${HPCKIT_PATH}/HPCKit/${HMPI_VERSION}/kml
     echo -e "已自动加载毕昇编译器、Hyper-MPI和鲲鹏数学库："
     module li
-else
-    echo -e "INFO: 检测到未安装 $HPCKIT_VERSION 版本的 HPCKit，请依次执行以下命令安装："
-    echo -e "./jarvis -use templates/basic_env/data.hpckit.config\n./jarvis -dp"
-fi
+}
 
+function check_hpckit() {
+    #判断hpckit是否安装，更新配套版本
+    file_path="${JARVIS_ROOT}/software/utils/hpckit/${HPCKIT_VERSION}/HPCKit/${HPCKIT_VERSION}/modulefiles/bisheng"
+    if [ -e "$file_path" ]; then
+        echo -e "你正在使用 $HPCKIT_VERSION 版本的 HPCKit"
+        load_hpckit
+    else
+        echo -e "INFO: 检测到未安装 $HPCKIT_VERSION 版本的 HPCKit，请依次执行以下命令安装："
+        echo -e "./jarvis -use templates/basic_env/data.hpckit.config\n./jarvis -dp"
+    fi
+}
+
+function check_network() {
+    curl -s --head --request GET https://gitee.com > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "[WARNNING] 当前网络异常，无法连接gitee，请检查网络"
+    fi
+}
+
+#检查编译器
+function check_com() {
+    ifclang=`cat .meta | awk -F '/' 'END{print $NF}'|grep clang`
+    ifgcc=`cat .meta | awk -F '/' 'END{print $NF}'|grep gcc`
+    if [ -n "$ifclang" ]; then
+        return 1
+    elif [ -n "$ifgcc" ]; then
+        return 2
+    else
+        return 0
+    fi
+}
+function check_deps() {
+    ifdep`rpm -qa|grep flex`
+    if [ -n "$ifsme" ]; then
+        echo "[INFO] deps are installed"
+    else
+        yum -y install git time zlib zlib-devel gcc gcc-c++ environment-modules python python3 python3-devel python3-libs python3-pip 
+        yum -y install cmake make numactl numactl-devel numactl-libs rpmdevtools wget libtirpc libtirpc-devel unzip flex 
+        yum -y install tar patch glibc-devel rpcbind csh perl-XML-LibXML xorg-x11-xauth curl curl-devel libcurl-devel
+    fi
+}
+
+#命令执行
+check_network
+check_deps
+check_com
+if [ $? -eq 1 ]; then
+    echo "using bisheng complier"
+    check_hpckit
+elif [ $? -eq 2 ]; then
+    echo "using gcc complier"
+else
+    echo ""
+fi
 
 #Install modules
 if ! type module >/dev/null 2>&1;then
