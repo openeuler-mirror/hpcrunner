f8a9e07ba69c94b13e6cef2b15095f69684cd15c
yyu497 <hellocxu@163.com>
2026-01-13 20:06:26 +0800

    修复代码缩进问题

--- src/installService.py
+++ src/installService.py
@@ -426,12 +426,12 @@ chmod +x {install_script}
             print("install failed")
             sys.exit(1)
 
-     def ensure_symlink(self, real_path, logical_path):
- 	         if os.path.islink(logical_path) or os.path.exists(logical_path):
- 	             return
- 	         self.tool.mkdirs(os.path.dirname(logical_path))
- 	         os.symlink(real_path, logical_path)
- 	 
+    def ensure_symlink(self, real_path, logical_path):
+        if os.path.islink(logical_path) or os.path.exists(logical_path):
+            return
+        self.tool.mkdirs(os.path.dirname(logical_path))
+        os.symlink(real_path, logical_path)
+
     def add_install_info(self, software_info, install_path):
         software_dict = {}
         software_dict['name'] = software_info['sname']
@@ -475,15 +475,16 @@ chmod +x {install_script}
         
         # get install path
         logical_install_path = self.get_install_path(software_info, env_info)
- 	    if not logical_install_path: return
- 	    if prefix:
- 	        real_install_path = logical_install_path.replace(self.SOFTWARE_PATH, os.path.join(prefix, "software"), 1)
- 	    else:
- 	        real_install_path = logical_install_path
+        if not logical_install_path:
+            return
+        if prefix:
+            real_install_path = logical_install_path.replace(self.SOFTWARE_PATH, os.path.join(prefix, "software"), 1)
+        else:
+            real_install_path = logical_install_path
         # get install script
         self.install_package(abs_software_path, real_install_path, other_args)
- 	    # create soft link from realpath to hpcrunner/software
- 	    self.ensure_symlink(real_install_path, logical_install_path)
+        # create soft link from realpath to hpcrunner/software
+        self.ensure_symlink(real_install_path, logical_install_path)
         # add install info
         self.add_install_info(software_info, logical_install_path)
         # gen module file
