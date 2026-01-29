473230915d26b144f1a089698f12ec4dff3f7341
yyyaaa222 <2393110978@qq.com>
2026-01-29 18:09:11 +0800

    update templates/CNVnator/0.4.1/cnvnator_test.sh

--- templates/CNVnator/0.4.1/cnvnator_test.sh
+++ templates/CNVnator/0.4.1/cnvnator_test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd ${JARVIS_TMP_DOWNLOAD}/CNVnator_v0.4.1/ExampleData/
+cd ${JARVIS_TMP}/CNVnator_v0.4.1/ExampleData/
 #该测试BAM文件仅作为cnvnator命令使用参考的小demo。若需包含拷贝数变异CNV测试请自行以实际为准
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/chr20.fa.gz
 wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/NA12878/alignment/NA12878.chrom20.ILLUMINA.bwa.CEU.low_coverage.20121211.bam
