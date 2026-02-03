679576bb008cec1084d8d86ca82d797749723ce1
yyyaaa222 <2393110978@qq.com>
2026-02-03 14:57:49 +0800

    add package/gsl/1.15/install.sh

---
+++ package/gsl/1.15/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -x
+set -e
+. ${DOWNLOAD_TOOL} -u http://mirrors.ustc.edu.cn/gnu/gsl/gsl-1.15.tar.gz
+cd ${JARVIS_TMP}
+rm -rf gsl-1.15
+tar -xvf ${JARVIS_DOWNLOAD}/gsl-1.15.tar.gz
+cd gsl-1.15
+./configure --build=aarch64-unknown-linux-gnu --prefix=$1
+make -j
+make install
+
