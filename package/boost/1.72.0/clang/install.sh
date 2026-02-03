8843a989395555035d507dd3ebee68e6c286b97a
Zerry <996002763@qq.com>
2026-02-03 22:30:32 +0800

    更新boost下载链接

--- package/boost/1.72.0/clang/install.sh
+++ package/boost/1.72.0/clang/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 set -e
-. ${DOWNLOAD_TOOL} -u https://nchc.dl.sourceforge.net/project/boost/boost/1.72.0/boost_1_72_0.tar.bz2
+. ${DOWNLOAD_TOOL} -u https://sourceforge.net/projects/boost/files/boost/1.72.0/boost_1_72_0.tar.gz
 cd ${JARVIS_TMP}
-tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.bz2
+tar -xvf ${JARVIS_DOWNLOAD}/boost_1_72_0.tar.gz
 cd boost_1_72_0
 sed -i '60s/.*/#ifdef PTHREAD_STACK_MIN/' ./boost/thread/pthread/thread_data.hpp
 ./bootstrap.sh --with-toolset=clang --with-libraries=iostreams,filesystem,timer,chrono,program_options,regex,system,serialization
