c2be53e6c4e9120c29882cd7044170071104a8e6
yyyaaa222 <2393110978@qq.com>
2026-02-03 11:26:13 +0800

    add package/lapack/3.12.0

---
+++ package/lapack/3.12.0/bisheng/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -x
+set -e
+. $DOWNLOAD_TOOL -u $JARVIS_PROXY/Reference-LAPACK/lapack/archive/refs/tags/v3.12.0.tar.gz -f lapack-3.12.0.tar.gz
+cd ${JARVIS_TMP}
+tar -xvf ${JARVIS_DOWNLOAD}/lapack-3.12.0.tar.gz
+cd lapack-3.12.0
+cp make.inc.example make.inc
+sed -i '9s/gcc/clang/g' ./make.inc
+sed -i '10s/$/& -march=native/g' ./make.inc
+sed -i '20s/gfortran/flang/g' ./make.inc
+sed -i '21s/-frecursive$//g' ./make.inc
+sed -i '23s/-frecursive$//g' ./make.inc
+sed -i '30s/gfortran/flang/g' ./make.inc
+sed -i '46s/^/#  /g' ./make.inc
+sed -i '50s/^#//g' ./make.inc
+rm build -rf
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=$1 -DBUILD_SHARED_LIBS=ON
+make -j $(nproc)
+make install
+ln -s $1/lib64 $1/lib
+
+cd ..
+sed -i "s|\$(TOPSRCDIR)|$1/lib64|g" make.inc
+make -j $(nproc) blaslib
+
