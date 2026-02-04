90a171a75b7fa1f5bd8602b54d82bc17d6b96cf6
Zerry <996002763@qq.com>
2026-02-04 17:17:46 +0800

    update: 更新文件 install.sh
    Signed-off-by: Zerry <996002763@qq.com>

--- package/spack/1.0.0/install.sh
+++ package/spack/1.0.0/install.sh
@@ -2,9 +2,7 @@
 set -x
 set -e
 . ${DOWNLOAD_TOOL} -u $JARVIS_PROXY/spack/spack/archive/refs/tags/v1.0.0-alpha.4.tar.gz -f spack-1.0.0.tar.gz
-cd ${JARVIS_ROOT}/tmp
-if [ ! -d 'spack' ]; then
-tar -xvf ${JARVIS_DOWNLOAD}/spack-1.0.0.tar.gz
-mv spack-1.0.0-alpha.4 spack
-fi
 cd ${JARVIS_ROOT}
+tar -xvf ${JARVIS_DOWNLOAD}/spack-1.0.0.tar.gz
+mv spack-1.0.0-alpha.4/* $1
+rm -rf spack-1.0.0-alpha.4
