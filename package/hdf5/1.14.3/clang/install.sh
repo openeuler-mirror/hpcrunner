6415ea4e8c0ffe0bb113d3c8b71f66074c6c5cf8
yyyaaa222 <2393110978@qq.com>
2026-02-04 09:27:23 +0800

    update

--- package/hdf5/1.14.3/clang/install.sh
+++ package/hdf5/1.14.3/clang/install.sh
@@ -9,6 +9,7 @@ cd ${JARVIS_TMP}
 rm -rf hdfsrc
 tar -xvf ${JARVIS_DOWNLOAD}/hdf5-${hdf5_version}.tar.gz
 cd hdfsrc
+# export CC=mpicc CXX=mpicxx FC=mpif90 F77=mpif90
 ./configure --prefix=$1 --enable-fortran --enable-static=yes --with-zlib=/usr --enable-parallel --enable-shared CFLAGS="-O3 -fPIC -Wno-incompatible-pointer-types-discards-qualifiers -Wno-non-literal-null-conversion" FCFLAGS="-O3 -fPIC" LDFLAGS="-Wl,--build-id"
 sed -i '11835c wl="-Wl,"' libtool
 make -j
