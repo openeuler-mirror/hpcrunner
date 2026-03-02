a17ef28787718496c2f7989f29f3e8e17cd1536d
gao-wenyao123 <2794580162@qq.com>
2026-03-02 17:14:24 +0800

    ncview

--- package/ncview/2.1.7/install.sh
+++ package/ncview/2.1.7/install.sh
@@ -13,7 +13,7 @@ cd ncview-2.1.7
 NETCDF_DIR=${1%/*/*}/netcdf-clang/4.7.0
 UDUNITS_DIR=${1%/*/*}/udunits/2.2.28
 
-CC=mpicc CXX=mpicxx FC=mpifort ./configure LDFLAGS="-L${NETCDF_CLANG_PATH}/lib -L${HDF5_CLANG_PATH}/lib -L${PNETCDF_PATH}/lib" CPPFLAGS="-I${NETCDF_CLANG_PATH}/include -I${HDF5_CLANG_PATH}/include -I${PNETCDF_PATH}/include" --prefix=${JARVIS_ROOT}/software/apps/bisheng/ncview/2.1.7 --with-nc-config=${NETCDF_CLANG_PATH}/bin/nc-config --with-udunits2_incdir=${UDUNITS_PATH}/include --with-udunits2_libdir=${UDUNITS_PATH}/lib CFLAGS="-Wno-implicit-function-declaration -Wno-implicit-int -Wno-int-conversion"
+CC=mpicc CXX=mpicxx FC=mpifort ./configure LDFLAGS="-L${NETCDF_CLANG_PATH}/lib -L${HDF5_CLANG_PATH}/lib -L${PNETCDF_PATH}/lib" CPPFLAGS="-I${NETCDF_CLANG_PATH}/include -I${HDF5_CLANG_PATH}/include -I${PNETCDF_PATH}/include" --prefix=${JARVIS_ROOT}/software/apps/bisheng${BISHENG_VERSION}/ncview/2.1.7 --with-nc-config=${NETCDF_CLANG_PATH}/bin/nc-config --with-udunits2_incdir=${UDUNITS_PATH}/include --with-udunits2_libdir=${UDUNITS_PATH}/lib CFLAGS="-Wno-implicit-function-declaration -Wno-implicit-int -Wno-int-conversion"
 
 make -j
 make install
