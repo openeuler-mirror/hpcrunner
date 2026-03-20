238af9a0163c8eafc3495a7dfeda664f17cc03a1
yyyaaa222 <2393110978@qq.com>
2026-03-20 15:56:15 +0800

    update package/blas/3.10.0/clang/install.sh

--- package/blas/3.10.0/clang/install.sh
+++ package/blas/3.10.0/clang/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #http://www.netlib.org/blas/blas-3.10.0.tgz
+<< aaa
 set -x
 set -e
 . ${DOWNLOAD_TOOL} -u http://www.netlib.org/blas/blas-3.10.0.tgz
@@ -11,3 +12,20 @@ cd BLAS-3.10.0
 ar rv libblas.a *.o
 mkdir $1/lib
 cp libblas.a $1/lib
+aaa
+
+set -x
+set -e
+. "${DOWNLOAD_TOOL}" -u http://www.netlib.org/blas/blas-3.10.0.tgz
+cd "${JARVIS_TMP}"
+rm -rf BLAS-3.10.0
+tar -xzvf "${JARVIS_DOWNLOAD}"/blas-3.10.0.tgz
+cd BLAS-3.10.0
+rm build -rf
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX=$1 -DBUILD_SHARED_LIBS=ON
+make -j $(nproc)
+make install
+
+
