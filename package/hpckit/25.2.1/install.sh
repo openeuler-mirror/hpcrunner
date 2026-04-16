572c6ee75614a0e28ae9821e7f34b4baf17e95e4
wangjianming <wangjianming16@huawei.com>
2026-04-16 11:07:42 +0800

    fix: correct syntax error in auto-generated modulefile

--- package/hpckit/25.2.1/install.sh
+++ package/hpckit/25.2.1/install.sh
@@ -2,3 +2,12 @@
 set -e
 export hpckit_ver="25.2.1"
 ../meta.sh $1
+MODULEFILE_PATH=$1/HPCKit/25.2.1/modulefiles/bisheng/kml25.2.1/kblas
+if [ -f "$MODULEFILE_PATH/multi" ]; then
+    sed -i '71s|fi|}|' $MODULEFILE_PATH/multi
+    sed -i '70s|fi|}|' $MODULEFILE_PATH/serial-locking
+    sed -i '70s|fi|}|' $MODULEFILE_PATH/serial-nolocking
+    echo "Successfuly patched modulefile syntax"
+else
+    echo "Warning : Modulefile not found at $MODULEFILE_PATH/multi"
+fi
