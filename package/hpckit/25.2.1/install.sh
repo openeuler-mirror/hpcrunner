9f5840c8b74436201716c719988752b1ed282c8a
wangjianming <wangjianming16@huawei.com>
2026-04-24 17:55:41 +0800

    fix: check all modulefile variants exist before patching syntax and fix typo in log message

--- package/hpckit/25.2.1/install.sh
+++ package/hpckit/25.2.1/install.sh
@@ -3,11 +3,11 @@ set -e
 export hpckit_ver="25.2.1"
 ../meta.sh $1
 MODULEFILE_PATH=$1/HPCKit/25.2.1/modulefiles/bisheng/kml25.2.1/kblas
-if [ -f "$MODULEFILE_PATH/multi" ]; then
+if [ -f "$MODULEFILE_PATH/multi" ] && [ -f "$MODULEFILE_PATH/serial-locking" ] && [ -f "$MODULEFILE_PATH/serial-nolocking" ]; then
     sed -i '71s|fi|}|' $MODULEFILE_PATH/multi
     sed -i '70s|fi|}|' $MODULEFILE_PATH/serial-locking
     sed -i '70s|fi|}|' $MODULEFILE_PATH/serial-nolocking
-    echo "Successfuly patched modulefile syntax"
+    echo "Successfully patched modulefile syntax"
 else
-    echo "Warning : Modulefile not found at $MODULEFILE_PATH/multi"
+    echo "Warning : Modulefile not found at $MODULEFILE_PATH/multi, $MODULEFILE_PATH/serial-locking and $MODULEFILE_PATH/serial-nolocking"
 fi
